b91655a87ea08559106080ab2af891099059d067
jiabao hu <hujb2000@163.com>
2018-09-07 01:20:30 +0800

    temp

--- scripts/start_mongodb.bat
+++ scripts/start_mongodb.bat
@@ -1,2 +1,3 @@
 rem: docker run --restart=always --name my-mongo -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -e MONGO_INITDB_DATABASE=news -v I:\docker_op\mongo_test:/etc/mongo  -d mongo:4.0.1-xenial --config /etc/mongo/mongod.conf
-docker run --restart=always --name my-mongo -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -e MONGO_INITDB_DATABASE=news -v I:\docker_op\mongo_test:/etc/mongo  -d mongo:4.0.1-xenial 
+rem: H:\databases\mongodb\db  >docker cp h:\databases\mongodb\db 5b:/data; cdata/onfigdb  db 
+docker run --restart=always --name my-mongo -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -e MONGO_INITDB_DATABASE=news   -v H:\databases\mongodb\db:/data/db -d mongo:4.0.1-xenial 
