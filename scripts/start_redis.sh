ba6cf5b9f583737931b80b37524ed65f6cb185a4
allen.hu <hujb2000@163.com>
2019-06-11 00:06:18 +0800

    test

--- scripts/start_redis.sh
+++ scripts/start_redis.sh
@@ -1,1 +1,1 @@
-docker run -d --name redis1 -h redis1  -p 6379:6379  -v /Users/hujiabao/Downloads/docker_op/redis_test/redis_origin.conf:/etc/redis.conf -v /Users/hujiabao/Downloads/docker_op/redis_test/db/:/data redis redis-server
+docker run -d --name redis1   -p 6379:6379  -v /root/docker_op/redis_test/redis.conf:/etc/redis.conf -v /root/docker_op/redis_test/db/:/data redis redis-server --requirepass "123456787654321"
