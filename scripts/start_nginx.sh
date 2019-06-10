3c61594961ec7e35c0d92447c59acd868472b9e1
allen.hu <hujb2000@163.com>
2019-06-10 23:12:16 +0800

    tem

--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -1,1 +1,1 @@
-docker run -d  --name nginx -v /Users/hujiabao/workspace_tagent/tagent-f:/site -v /Users/hujiabao/Downloads/docker_op/nginx1.2.1_conf:/etc/nginx/ -p 80:80  nginx
+docker run -d  --name nginx  -v /root/docker_op/nginx1.17.0:/etc/nginx/ -p 80:80  nginx
