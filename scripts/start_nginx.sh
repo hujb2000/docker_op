f13e15363aa73d03dceca9059fc2a30f0178f147
hujb2000 <hujb2000@163.com>
2020-05-12 11:44:14 +0800

    test

--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -1,1 +1,1 @@
-docker run -d  --name nginx  -v /root/docker_op/nginx1.17.0:/etc/nginx/ -p 80:80  nginx
+docker run -d  --name nginx  -v /Users/hujiabao/workspace/docker_op/nginx1.17.0:/etc/nginx/ -v /Users/hujiabao/workspace/ai2boss/agent-f/dist:/usr/share/nginx/html -p 80:80  nginx
